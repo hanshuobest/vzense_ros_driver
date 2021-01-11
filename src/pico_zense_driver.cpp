03262eb04f299df5fc018f77d610cc454570abfb
hanshuobest <hscoder@163.com>
2021-01-11 15:43:11 +0800

    update

--- src/pico_zense_driver.cpp
+++ src/pico_zense_driver.cpp
@@ -6,20 +6,24 @@
 #include "sensor_msgs/image_encodings.h"
 #include <sensor_msgs/CameraInfo.h>
 
-namespace autolabor_driver {
-    PicoZenseDriver::PicoZenseDriver() : _session_index(0) {
-
+namespace autolabor_driver
+{
+    PicoZenseDriver::PicoZenseDriver() : _session_index(0)
+    {
     }
 
-    PicoZenseDriver::~PicoZenseDriver() {
-        if (_device_handle != nullptr) {
+    PicoZenseDriver::~PicoZenseDriver()
+    {
+        if (_device_handle != nullptr)
+        {
             Ps2_StopStream(_device_handle, _session_index);
             Ps2_CloseDevice(_device_handle);
             Ps2_Shutdown();
         }
     }
 
-    void PicoZenseDriver::initParams() {
+    void PicoZenseDriver::initParams()
+    {
         ros::NodeHandle private_nh("~");
         private_nh.param<std::string>("namespace", _namespace, "pico_camera");
         private_nh.param<std::string>("frame_name", _frame_name, "pico_camera");
@@ -30,19 +34,20 @@ namespace autolabor_driver {
         private_nh.param<int>("skip_row", _skip_row, 0);
         private_nh.param<int>("skip_column", _skip_column, 0);
 
-
-
         private_nh.param<bool>("output_depth_image", _output_depth_image, false);
         ros::NodeHandle nh(_namespace);
-        if (_output_depth_image) {
+        if (_output_depth_image)
+        {
             _depth_image_pub = nh.advertise<sensor_msgs::Image>("depth_image", 5);
         }
         private_nh.param<bool>("output_color_image", _output_color_image, false);
-        if (_output_color_image) {
+        if (_output_color_image)
+        {
             _color_image_pub = nh.advertise<sensor_msgs::Image>("color_image", 5);
         }
         private_nh.param<bool>("output_point_cloud", _output_point_cloud, true);
-        if (_output_point_cloud) {
+        if (_output_point_cloud)
+        {
             _point_cloud_pub = nh.advertise<sensor_msgs::PointCloud>("point_cloud", 5);
         }
 
@@ -58,37 +63,46 @@ namespace autolabor_driver {
         private_nh.param<bool>("depth_straighten_correction", _depth_straighten_correction, true);
     }
 
-    bool PicoZenseDriver::initCamera() {
+    bool PicoZenseDriver::initCamera()
+    {
         // Init SDK
-        if ((_status = Ps2_Initialize()) != PsRetOK) {
+        if ((_status = Ps2_Initialize()) != PsRetOK)
+        {
             ROS_ERROR("Ps2_Initialize error, error code : %d", _status);
             return false;
         }
         // Get Count
         uint32_t device_count = 0;
-        if ((_status = Ps2_GetDeviceCount(&device_count)) != PsRetOK) {
+        if ((_status = Ps2_GetDeviceCount(&device_count)) != PsRetOK)
+        {
             ROS_ERROR("Ps2_GetDeviceCount error, error code : %d", _status);
             return false;
-        } else if (device_count == 0) {
+        }
+        else if (device_count == 0)
+        {
             ROS_ERROR("No Device connected!");
             return false;
         }
 
         // Open Device
-        if (_device_index < device_count) {
+        if (_device_index < device_count)
+        {
             PsDeviceInfo device_info;
             Ps2_GetDeviceInfo(&device_info, _device_index);
-            if ((_status = Ps2_OpenDevice(device_info.uri, &_device_handle)) != PsRetOK) {
+            if ((_status = Ps2_OpenDevice(device_info.uri, &_device_handle)) != PsRetOK)
+            {
                 ROS_ERROR("Ps2_OpenDevice error, error code : %d", _status);
                 return false;
             }
 
-            if ((_status = Ps2_StartStream(_device_handle, _session_index)) != PsRetOK) {
+            if ((_status = Ps2_StartStream(_device_handle, _session_index)) != PsRetOK)
+            {
                 ROS_ERROR("Ps2_StartStream error, error code : %d", _status);
                 return false;
             }
 
-            if (configCamera()) {
+            if (configCamera())
+            {
                 return true;
             }
         }
@@ -96,48 +110,53 @@ namespace autolabor_driver {
         return false;
     }
 
-    bool PicoZenseDriver::configCamera() {
+    bool PicoZenseDriver::configCamera()
+    {
         // Set Data Mode
-        if (_output_color_image) {
+        if (_output_color_image)
+        {
             Ps2_SetDataMode(_device_handle, _session_index, PsDepthAndRGB_30);
             Ps2_SetColorPixelFormat(_device_handle, _session_index, PsPixelFormatBGR888);
-        } else {
+        }
+        else
+        {
             Ps2_SetDataMode(_device_handle, _session_index, PsDepthAndIR_30);
             Ps2_SetIrFrameEnabled(_device_handle, _session_index, false);
         }
 
         PsDepthRange depthRange;
-        switch (std::max(0, std::min(8, _depth_range))) {
-            case 0:
-                depthRange = PsNearRange;
-                break;
-            case 1:
-                depthRange = PsMidRange;
-                break;
-            case 2:
-                depthRange = PsFarRange;
-                break;
-            case 3:
-                depthRange = PsXNearRange;
-                break;
-            case 4:
-                depthRange = PsXMidRange;
-                break;
-            case 5:
-                depthRange = PsXFarRange;
-                break;
-            case 6:
-                depthRange = PsXXNearRange;
-                break;
-            case 7:
-                depthRange = PsXXMidRange;
-                break;
-            case 8:
-                depthRange = PsXXFarRange;
-                break;
-            default:
-                ROS_ERROR("something wrong");
-                return false;
+        switch (std::max(0, std::min(8, _depth_range)))
+        {
+        case 0:
+            depthRange = PsNearRange;
+            break;
+        case 1:
+            depthRange = PsMidRange;
+            break;
+        case 2:
+            depthRange = PsFarRange;
+            break;
+        case 3:
+            depthRange = PsXNearRange;
+            break;
+        case 4:
+            depthRange = PsXMidRange;
+            break;
+        case 5:
+            depthRange = PsXFarRange;
+            break;
+        case 6:
+            depthRange = PsXXNearRange;
+            break;
+        case 7:
+            depthRange = PsXXMidRange;
+            break;
+        case 8:
+            depthRange = PsXXFarRange;
+            break;
+        default:
+            ROS_ERROR("something wrong");
+            return false;
         }
         Ps2_SetDSPEnabled(_device_handle, _session_index, true);
         Ps2_SetDepthRange(_device_handle, _session_index, depthRange);
@@ -165,9 +184,11 @@ namespace autolabor_driver {
         return true;
     }
 
-    void PicoZenseDriver::publishColorImage() {
+    void PicoZenseDriver::publishColorImage()
+    {
         if (_output_color_image && ready_.rgb &&
-            Ps2_GetFrame(_device_handle, _session_index, PsRGBFrame, &_color_frame) == PsRetOK) {
+            Ps2_GetFrame(_device_handle, _session_index, PsRGBFrame, &_color_frame) == PsRetOK)
+        {
             sensor_msgs::Image img_msg;
             img_msg.header.frame_id = _frame_name + "_color_frame";
             img_msg.header.stamp = ros::Time::now();
@@ -178,35 +199,19 @@ namespace autolabor_driver {
             img_msg.step = img_msg.width * 3;
             int len = img_msg.width * img_msg.height * 3;
             img_msg.data.resize(len);
-            for (int i = 0; i < len; i++) {
+            for (int i = 0; i < len; i++)
+            {
                 img_msg.data[i] = _color_frame.pFrameData[i];
             }
             _color_image_pub.publish(img_msg);
         }
     }
 
-    void PicoZenseDriver::publishDepthImage() 
+    void PicoZenseDriver::publishDepthImage()
     {
-        // if (_output_depth_image && ready_.depth &&
-        //     Ps2_GetFrame(_device_handle, _session_index, PsDepthFrame, &_depth_frame) == PsRetOK) {
-        //     sensor_msgs::Image img_msg;
-        //     img_msg.header.frame_id = _frame_name + "_depth_frame";
-        //     img_msg.header.stamp = ros::Time::now();
-        //     img_msg.width = _depth_frame.width;
-        //     img_msg.height = _depth_frame.height;
-        //     img_msg.is_bigendian = false;
-        //     img_msg.encoding = sensor_msgs::image_encodings::MONO16;
-        //     img_msg.step = img_msg.width * 2;
-        //     int len = img_msg.width * img_msg.height * 2;
-        //     img_msg.data.resize(len);
-        //     for (int i = 0; i < len; i++) {
-        //         img_msg.data[i] = _depth_frame.pFrameData[i];
-        //     }
-        //     _depth_image_pub.publish(img_msg);
-        // }
-
         if (_output_depth_image && ready_.mappedDepth &&
-            Ps2_GetFrame(_device_handle, _session_index, PsDepthFrame, &_depth_frame) == PsRetOK) {
+            Ps2_GetFrame(_device_handle, _session_index, PsMappedDepthFrame, &_depth_frame) == PsRetOK)
+        {
             sensor_msgs::Image img_msg;
             img_msg.header.frame_id = _frame_name + "_depth_frame";
             img_msg.header.stamp = ros::Time::now();
@@ -217,28 +222,36 @@ namespace autolabor_driver {
             img_msg.step = img_msg.width * 2;
             int len = img_msg.width * img_msg.height * 2;
             img_msg.data.resize(len);
-            for (int i = 0; i < len; i++) {
+            for (int i = 0; i < len; i++)
+            {
                 img_msg.data[i] = _depth_frame.pFrameData[i];
             }
             _depth_image_pub.publish(img_msg);
         }
     }
 
-    void PicoZenseDriver::publishPointCloud() {
-        if (_output_point_cloud) {
-            if (ready_.depth && !_output_depth_image) {
+    void PicoZenseDriver::publishPointCloud()
+    {
+        if (_output_point_cloud)
+        {
+            if (ready_.depth && !_output_depth_image)
+            {
                 Ps2_GetFrame(_device_handle, _session_index, PsDepthFrame, &_depth_frame);
             }
             PsVector3f point[_depth_frame.width * _depth_frame.height];
             _status = Ps2_ConvertDepthFrameToWorldVector(_device_handle, _session_index, _depth_frame, point);
-            if (_status == PsRetOK) {
+            if (_status == PsRetOK)
+            {
                 sensor_msgs::PointCloud point_msg;
                 point_msg.header.frame_id = _frame_name + "_depth_frame";
                 point_msg.header.stamp = ros::Time::now();
-                for (int i = 0; i < _depth_frame.height; i = i + 1 + _skip_row) {
-                    for (int j = 0; j < _depth_frame.width; j = j + 1 + _skip_column) {
+                for (int i = 0; i < _depth_frame.height; i = i + 1 + _skip_row)
+                {
+                    for (int j = 0; j < _depth_frame.width; j = j + 1 + _skip_column)
+                    {
                         size_t index = i * _depth_frame.width + j;
-                        if (point[index].x != 0 || point[index].y != 0 || point[index].z != 0) {
+                        if (point[index].x != 0 || point[index].y != 0 || point[index].z != 0)
+                        {
                             geometry_msgs::Point32 p;
                             p.x = point[index].x / 1000.0;
                             p.y = point[index].y / 1000.0;
@@ -323,7 +336,7 @@ namespace autolabor_driver {
         // cam_info.width = 640;
         // cam_info.height = 480;
 >>>>>>> abf88e6d4b13139dc7293e422582856a0c5a4498
-        
+
         cam_info.header.frame_id = "camera_color_optical_frame";
         cam_info.K.at(0) = camera_params.fx;
         cam_info.K.at(1) = 0.0;
@@ -379,7 +392,7 @@ namespace autolabor_driver {
 
         std::cout << "step 5 is ok-------------------\n";
 
-        if(_output_color_info)
+        if (_output_color_info)
 <<<<<<< HEAD
 =======
         {
@@ -412,55 +425,51 @@ namespace autolabor_driver {
 
     void PicoZenseDriver::publishDepthImage()
     {
-        if (_output_depth_image && ready_.depth &&
+        if (_output_depth_image && ready_.mappedDepth &&
             Ps2_GetFrame(_device_handle, _session_index, PsDepthFrame, &_depth_frame) == PsRetOK)
         {
-            _color_info_pub.publish(cam_info);
+            sensor_msgs::Image img_msg;
+            img_msg.header.frame_id = _frame_name + "_depth_frame";
+            img_msg.header.stamp = ros::Time::now();
+            img_msg.width = _depth_frame.width;
+            img_msg.height = _depth_frame.height;
+            img_msg.is_bigendian = false;
+            img_msg.encoding = sensor_msgs::image_encodings::MONO16;
+            img_msg.step = img_msg.width * 2;
+            int len = img_msg.width * img_msg.height * 2;
+            img_msg.data.resize(len);
+            for (int i = 0; i < len; i++)
+            {
+                img_msg.data[i] = _depth_frame.pFrameData[i];
+            }
+            _depth_image_pub.publish(img_msg);
         }
     }
 
-
-    void PicoZenseDriver::run() {
+    void PicoZenseDriver::run()
+    {
         initParams();
         ros::Duration duration(_read_frame_interval / 1000.0);
-        if (initCamera()) {
+        if (initCamera())
+        {
             publishTf();
-            while (ros::ok()) {
-                if (Ps2_ReadNextFrame(_device_handle, _session_index, &ready_) == PsRetOK) {
-
             while (ros::ok())
             {
-
-                Timer timer_obj;
-                timer_obj.Tic();
-                PsReturnStatus status = Ps2_ReadNextFrame(_device_handle, _session_index, &ready_);
-                timer_obj.Toc();
-
-                std::cout << "read frame cost time: " << timer_obj.Elasped() << std::endl;
-                if (status == PsRetOK)
+                if (Ps2_ReadNextFrame(_device_handle, _session_index, &ready_) == PsRetOK)
                 {
-                    timer_obj.Tic();
+
                     publishColorImage();
+                    //publishColorInfo();
                     publishDepthImage();
                     publishPointCloud();
-                    // publishColorInfo();
                 }
                 duration.sleep();
-
-                    timer_obj.Tic();
-                    timer_obj.Toc();
-                    publishColorInfo();
-                    std::cout << "publish color info cost time: " << timer_obj.Elasped() << std::endl;
-                }
-
-                //duration.sleep();
-                //ros::spinOnce();
-                loop_rate.sleep();
             }
         }
     }
 
-    void PicoZenseDriver::publishTf() {
+    void PicoZenseDriver::publishTf()
+    {
         Ps2_GetCameraExtrinsicParameters(_device_handle, _session_index, &_camera_ep);
         tf::Matrix3x3 rotation_matrix(_camera_ep.rotation[0], _camera_ep.rotation[1], _camera_ep.rotation[2],
                                       _camera_ep.rotation[3], _camera_ep.rotation[4], _camera_ep.rotation[5],
@@ -488,7 +497,8 @@ namespace autolabor_driver {
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     ros::init(argc, argv, "PicoZenseDriver");
     autolabor_driver::PicoZenseDriver picoZenseDriver;
     picoZenseDriver.run();
